fcd3501b8b9a694c7c62c65504dac9a17f89a41b
satori <liwei272558338@163.com>
2019-11-24 16:12:28 +0800

    'phone-beizhu-11-24'

--- wuhan_tel.py
+++ wuhan_tel.py
@@ -4,10 +4,12 @@ if __name__ == "__main__":
     #打开csv
     ff = open('tel.csv', 'a', newline='')
     writer = csv.writer(ff)
+    # 打开txt
     with open('520.txt','r') as f:
         tel_list = []
         for num in f:
             # print(num.strip())
+            #把号码放入函数转换成字典
             info = Phone().find(num.strip())
             print(info)
             try:
@@ -17,12 +19,13 @@ if __name__ == "__main__":
                 zip_code = info['zip_code']
                 area_code = info['area_code']
                 phone_type = info['phone_type']
+                # 把字典里的值放入元组添加到列表
                 tel_list.append(
                     (phone,province,city,zip_code,area_code,phone_type)
                 )
-
             except:
                 print('none')
+        # 遍历结束后,把列表写入到csv(一次性全部写入)
         writer.writerows(tel_list)
     f.close()
     ff.close()
